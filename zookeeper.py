c13d989449ab1958d68a66700f4247b1b0afdbee
segaleden123 <79809843+segaleden123@users.noreply.github.com>
2022-11-03 18:45:56 +0200

    Update zookeeper.py

--- zookeeper.py
+++ zookeeper.py
@@ -32,57 +32,7 @@ class ZooKeeperREST_TestCase(unittest.TestCase):
             pass # it's ok if already exists
         assert self.zk.exists('/test') is True
 
-    def test_create_hierarchy(self):
-        try:
-            self.zk.delete(['/a/b', '/a'])
-        except ZooKeeper.NotFound:
-            pass
-
-        self.zk.create('/a')
-        self.zk.create('/a/b')
-
-        self.zk.delete(['/a/b', '/a'])
-
-    def test_create_with_data(self):
-        self.zk.create('/test', 'some-data')
-
-        zn = self.zk.get('/test')
-        self.assertEqual(zn.get('data64', None), \
-            'some-data'.encode('base64').strip())
-
-    def test_delete_znode(self):
-        self.zk.create('/test')
-
-        self.zk.delete('/test')
-        assert not self.zk.exists('/test')
-
-    def test_delete_older_version(self):
-        self.zk.create('/test')
-
-        zn = self.zk.get('/test')
- 
-        self.zk.set('/test', 'dummy-data')
-
-        self.assertRaises(ZooKeeper.WrongVersion, \
-            self.zk.delete, '/test', version=zn['version'])
-
-    def test_delete_raise_not_found(self):
-        self.zk.create('/test')
-
-        zn = self.zk.get('/test')
-        self.zk.delete('/test')
- 
-        self.assertRaises(ZooKeeper.NotFound, \
-            self.zk.delete, '/test', version=zn['version'])
-
-    def test_set(self):
-        self.zk.create('/test')
-
-        self.zk.set('/test', 'dummy')
-
-        self.assertEqual(self.zk.get('/test')['data64'], \
-            'dummy'.encode('base64').strip())
-
+   
     def test_set_with_older_version(self):
         if not self.zk.exists('/test'):
             self.zk.create('/test', 'random-data')
@@ -100,34 +50,7 @@ class ZooKeeperREST_TestCase(unittest.TestCase):
 
         self.zk.set('/test', null=True)
         assert 'data64' not in self.zk.get('/test')
-
-    def test_create_ephemeral_node(self):
-        with self.zk.session():
-            if self.zk.exists('/ephemeral-test'):
-                self.zk.delete('/ephemeral-test')
-
-            self.zk.create('/ephemeral-test', ephemeral=True)
-            zn = self.zk.get('/ephemeral-test')
-
-            assert zn['ephemeralOwner'] != 0
-
-    def test_create_session(self):
-        with self.zk.session() as sid:
-            self.assertEqual(len(sid), 36) # UUID
-
-    def test_session_invalidation(self):
-        self.zk.start_session(expire=1)
-        self.zk.create('/ephemeral-test', ephemeral=True)
-
-        # keep the session alive by sending heartbeat requests
-        for _ in range(1,2):
-            self.zk.heartbeat()
-            time.sleep(0.9)
-
-        time.sleep(2) # wait for the session to expire
-        self.assertRaises(ZooKeeper.InvalidSession, \
-            self.zk.create, '/ephemeral-test', ephemeral=True)
-
+        
     def test_presence_signaling(self):
         with self.zk.session(expire=1):
             self.zk.create('/i-am-online', ephemeral=True)
